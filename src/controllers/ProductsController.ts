a6d7c31c862b31aafc5d7918638ff0ae7f30a590
victor <victorhugoornellas@gmail.com>
2026-04-22 10:44:09 -0300

    Schema Validation

--- src/controllers/ProductsController.ts
+++ src/controllers/ProductsController.ts
@@ -1,6 +1,6 @@
-import { error } from "console"
 import { Request, Response} from "express"
 import { AppError } from "../utils/AppError"
+import { z } from "zod"
 
 class ProductsController {
     /*
@@ -17,15 +17,29 @@ class ProductsController {
             response.send(`Página ${page} de ${limit}`)
     }
 
-    create(request: Request, response: Response){
-        const { name, price } = request.body  
+    create(request: Request, response: Response){ 
+        const bodySchema = z.object({ 
+            // Campo obrigatorio!! Caso use o nullish o campo passa ser opcional
+            name: z
+                .string({ required_error: "Name is required!"})
+                .trim()
+                .min(6, { message: "Name must be 6 or more characteres"}),
+                
+            price: z
+            .number({ required_error: "Price is required!"})
+            .positive({message: "Price must be positive!"}),
+        })
+        const { name, price } = bodySchema.parse(request.body)
 
-        if(!name){
+
+    
+
+        /*if(!name){
             throw new AppError("Nome do produto é obrigatorio!")
         }
         if(!price){
             throw new AppError("Preço do produto é obrigatorio!")
-        }
+        }*/
 
 
         //throw new Error( "Erro ao tentar criar um produto" )
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
 import express, { Request, Response} from "express"
+import {ZodError } from "zod"
+
 import { routes } from "./routes"
 import { AppError } from "./utils/AppError"
 
@@ -12,9 +14,14 @@ app.use(routes)
 
 app.use((error: any, request: Request, response: Response, _: any) =>{
     if(error instanceof AppError){
-        return response.status(error.statusCode).json({ message: error.message})
+        return response.status(error.statusCode).json({ message: error.message })
     }
 
+    if(error instanceof ZodError){
+       return response
+       .status(400)
+       .json({ message: "Validation error!", issues: error.format() })
+    }
 
     response.status(500).json({ message: error.message })
 })
